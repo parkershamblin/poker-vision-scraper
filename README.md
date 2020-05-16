df47eb96f95758ea786be62aaac0034398be8ed7
parkersnow <parkercoleshamblin@gmail.com>
2020-05-16 12:04:13 -0400

    Instructions for how to download added to readme

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # vision-scraper
 
 Scraper for Run It Once [Vision]('https://www.runitonce.com/vision/').
+
+Instructions:
+1. Download this repo.
+2. Download [Chrome Driver v81]('https://chromedriver.storage.googleapis.com/index.html?path=81.0.4044.138/')
+3. Download the lastest version Python [here]('https://www.python.org/downloads/release/python-383/'). Scroll down to the 'Files' section and choose the version needed for your machine. I recommend installing using the 'Windows x86-64 executable installer' for Windows.
+4. Download [Sublime Text 3]('https://www.sublimetext.com/3')
+5. For Windows users now open up this repo folder which you downloaded in step one and type 'cmd' in the search bar and run 'pip install -r requirements.txt'
+
+![image](https://user-images.githubusercontent.com/53675680/82123878-ccb51080-9769-11ea-9555-d8cb93981946.png)
+![image](https://user-images.githubusercontent.com/53675680/82123925-0128cc80-976a-11ea-9be0-12f373b5bd79.png)
+6. Now click on the 'scraper.py' file and click 'open with' and choose 'Sublime Text'. If 'Sublime Text' does not appear then click 'Choose another app' and select 'Sublime Text' from the option list. If 'Sublime text' does not appear in that option list then click 'More apps' then 'Look for another app on this PC' and go to where you download Sublime Text and select it.
+
+![image](https://user-images.githubusercontent.com/53675680/82124358-9af17900-976c-11ea-9ad8-8b1720fb467a.png)
+
+7. Finally enter in your Run It Once username and password and hit 'Ctrl + B' to run the file. Proceed to play hands as you would normally and the results will be saved as an excel file titled 'RIO-Vision-Results.xlsx'.
+
+![image](https://user-images.githubusercontent.com/53675680/82124442-118e7680-976d-11ea-842f-a37f17637206.png)
--- scraper.py
+++ scraper.py
@@ -1,26 +1,53 @@
-# # # starter code https://stackoverflow.com/a/40325895/10699882
+# Input your Run It Once Log In Details here.
+username_input = "ENTER YOUR USERNAME HERE"
+password_input = "ENTER YOUR PASSWORD HERE"
 
 import time
+import win32api
+from os import path
 from selenium import webdriver
 from openpyxl import Workbook
+from openpyxl import load_workbook
 
 
-wb = Workbook()
-ws = wb.active
-ws.title = '100bb SRP COvsBTN'
-ws['A1'] = 'Board'
-ws['B1'] = 'Hand'
-ws['C1'] = 'Result'
+# Load workbook if it exist and if not create new workbook.
+if path.exists('RIO-Vision-Results.xlsx'):
+    wb = load_workbook('RIO-Vision-Results.xlsx')
+    ws = wb.active
+else:
+    wb = Workbook()
+    ws = wb.active
+    ws.title = 'RIO-Vision-Results'
+    ws['A1'] = 'Line'
+    ws['B1'] = 'Overview'
+    ws['C1'] = 'Board'
+    ws['D1'] = 'Hand'
+    ws['E1'] = 'Result'
 
 
 chromedriver_path = r"C:\Users\Parker\Desktop\chromedriver.exe"
 #create webdriver object and get url
 driver = webdriver.Chrome(chromedriver_path)
 driver.implicitly_wait(1)
-driver.get('https://www.runitonce.com/vision/')
+driver.get('https://www.runitonce.com/login/?next=/vision/')
 
-# wait a bit so I can log into Run It Once
-time.sleep(45)
+
+# wait a bit for Run It Once to load and allow us to login
+# if your computer or internet is slow you might want to increase
+# this number from 7 to 10-14
+time.sleep(7)
+
+# log into Run It Once
+username = driver.find_element_by_id('login-username')
+username.clear()
+username.send_keys(username_input)
+password = driver.find_element_by_id('login-password')
+password.clear()
+password.send_keys(password_input)
+driver.find_element_by_class_name('login-btn').click()
+
+
+previous_result = ""
 
 
 def reverse(s): 
@@ -31,42 +58,57 @@ def reverse(s):
 
 
 def scrape_vision():
-    scenario = driver.find_element_by_id('board-header').text
+    line = driver.find_element_by_id('board-header').text
     board = driver.find_element_by_class_name('board').get_attribute('data-texture')
 
     # find cards
     foo = driver.find_element_by_id('intro-text')
     cards = []  # store cards in empty list
 
-    # grabs image file path for our cards
-    for i in foo.find_elements_by_class_name('result-hand-graphical'):
-        cards.append(i.get_attribute('src'))
+    try:
+        # grabs image file path for our cards
+        for i in foo.find_elements_by_class_name('result-hand-graphical'):
+            cards.append(i.get_attribute('src'))
 
-    hand = []
-    for card in cards:
-        hand.append(card.replace(r"https://www.runitonce.com/static/img/visions/card-set/", "").replace(r".png?2.0", "").replace('/', ''))
-    # move suit to right side of card 
-    for card in range(len(hand)):
-        hand[card] = reverse(hand[card])
-    # covert hand from list to string
-    hand = ''.join(hand[:])
+        hand = []
+        for card in cards:
+            hand.append(card.replace(r"https://www.runitonce.com/static/img/visions/card-set/", "").replace(r".png?2.0", "").replace('/', ''))
+        # move suit to right side of card 
+        for card in range(len(hand)):
+            hand[card] = reverse(hand[card])
+        # covert hand from list to string
+        hand = ''.join(hand[:])
 
-    # grab result and print text
-    result = driver.find_element_by_id('practice-result')
-    if result.text:
-        # write results in workbook
-        ws.append([board, hand, result.text])
-        # save results to workbook
-        wb.save('results.xlsx') 
-        
-        print(result.text)
-        print(hand)
+        # grab result and print text
+        result = driver.find_element_by_id('practice-result')
+        if result.text:
+
+            # prevents duplicate results being written to worksheet when a user clicks while on the results page
+            global previous_result
+            if result.text != previous_result:
+                # TODO Fix issue with situation not being included in first hand result
+                # have to click on overview tab to get overview text
+                driver.find_element_by_id('overview-tab').click()
+                situation = driver.find_element_by_id('hand-graph-title-overview').text
+                # click back to situation tab so that user doesn't have to do it each time
+                driver.find_element_by_id('situation-tab').click()
+
+                # write results in workbook
+                ws.append([line, situation, board, hand, result.text])
+                # save results to workbook
+                wb.save('RIO-Vision-Results.xlsx') 
+
+                previous_result = result.text
+                
+                print(result.text)
+                print(hand)
+    except Exception as e:
+        print(f"An error occured. Here is the error: {e}")
+        pass
 
 
 def check_for_left_click():
     # Code to check if left or right mouse buttons were pressed
-    import win32api
-    import time
 
     state_left = win32api.GetKeyState(0x01)  # Left button down = 0 or 1. Button up = -127 or -128
 
@@ -78,7 +120,7 @@ def check_for_left_click():
             # print(a)
             if a < 0:
                 # print('Left Button Pressed')
-                time.sleep(0.2)
+                time.sleep(0.05)
                 scrape_vision()
                 
         time.sleep(0.001)
